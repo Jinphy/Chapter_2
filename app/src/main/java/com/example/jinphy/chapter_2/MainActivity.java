6ca98f26f33907dedd0e9f03859d9d7a1f471f23
Jinphy <709584408@qq.com>
2017-07-18 16:14:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jinphy/chapter_2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jinphy.chapter_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = ((TextView) findViewById(R.id.text_view));
+        textView.setOnClickListener(this::onClick);
+    }
+
+
+    public void onClick(View view) {
+
+    }
+}
